28bfec8cd4ae49d29745138534badd6ea50cc7f3
KatDev <g0jakubowski@gmail.com>
2023-02-27 22:33:14 +0100

    init

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import utime as time
 from machine import Pin
+from neopixel import NeoPixel
 
+np = NeoPixel(Pin(2),1)
 led = Pin("LED", Pin.OUT)
 
 
-async def blink(period_on=0.1, period_off=0.3, repetitions=1):
+def blink(period_on=0.1, period_off=0.3, repetitions=1, color='green', brightness=0.5):
+    colors = {'red': (1.0, 0.0, 0.0),
+              'green': (0.0, 1.0, 0.0),
+              'blue': (0.0, 0.0, 1.0)}
+
+    requested_param = tuple([int(x*brightness*255) for x in colors[color]])
+
     for i in range(repetitions):
-        led.high()
+        np[0] = requested_param
+        np.write()
         time.sleep(period_on)
-        led.low()
+        np[0] = (0, 0, 0)
+        np.write()
         time.sleep(period_off)
 
 while True:
