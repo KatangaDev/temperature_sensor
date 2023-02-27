2ecae0a2dd62f4275b7d74e23ffa26a6135e22d0
KatDev <g0jakubowski@gmail.com>
2023-02-27 23:28:00 +0100

    OTA?

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import utime as time
-from machine import Pin
+from machine import Pin,reset
 from neopixel import NeoPixel
+import senko
+import network
+
 
 np = NeoPixel(Pin(2),1)
-led = Pin("LED", Pin.OUT)
+# led = Pin("LED", Pin.OUT)
+
+with open("settings.txt","r") as f:
+    ssid,password = f.read().splitlines(False)
+
+time.sleep(3)
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+time.sleep(1)
+
+print(wlan.isconnected())
+
 
 
 def blink(period_on=0.1, period_off=0.3, repetitions=1, color='green', brightness=0.5):
@@ -21,5 +37,23 @@ def blink(period_on=0.1, period_off=0.3, repetitions=1, color='green', brightnes
         np.write()
         time.sleep(period_off)
 
+# OTA = senko.Senko(
+#   user="KatangaDev", # Required
+#   repo="temperature_sensor", # Required
+#   branch="ota_test_esp", # Optional: Defaults to "master"
+#   working_dir="", # Optional: Defaults to "app"
+#   files=["main.py"]
+# )
+
+GITHUB_URL = "https://github.com/KatangaDev/temperature_sensor/tree/ota_test_esp"
+OTA = senko.Senko(url=GITHUB_URL, files=["main.py"])
+
+if OTA.update():
+    print("Updated to the latest version! Rebooting...")
+    reset()
+
 while True:
-    blink(0.1,0.9)
+
+
+    blink(0.1,0.9, color='green')
+    blink(0.1, 0.9, color='red')
