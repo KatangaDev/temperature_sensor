37b048219902968dd3bd755104c533991172feaf
KatDev <g0jakubowski@gmail.com>
2023-03-13 21:59:03 +0100

    Rpi with OTA

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
 import utime as time
 from machine import Pin
-from neopixel import NeoPixel
-
-np = NeoPixel(Pin(2), 1)
 
 
-def blink(period_on=0.1, period_off=0.3, repetitions=1, color='green', brightness=0.5):
-    colors = {'red': (1.0, 0.0, 0.0),
-              'green': (0.0, 1.0, 0.0),
-              'blue': (0.0, 0.0, 1.0)}
+led = Pin("LED", Pin.OUT)
 
-    requested_param = tuple([int(x * brightness * 255) for x in colors[color]])
 
+def blink(period_on=0.1, period_off=0.3, repetitions=1):
     for i in range(repetitions):
-        np[0] = requested_param
-        np.write()
+        led.high()
         time.sleep(period_on)
-        np[0] = (0, 0, 0)
-        np.write()
+        led.low()
         time.sleep(period_off)
 
 
 while True:
-    blink(1, 2, color='green')
+    blink(0.2, 0.2)
 
